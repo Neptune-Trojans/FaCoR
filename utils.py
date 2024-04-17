53d4ce2bf7e0b2d22f6cb559199a017162a59d4f
Paul <paul@kinsame.com>
2024-04-17 16:37:05 +0300

    added device getter

--- utils.py
+++ utils.py
@@ -9,8 +9,9 @@ def np2tensor(arrays, device='cpu'):
     tensor = tensor.to(device)
     return tensor
 
-def mylog(*t,path = 'log.txt'):
-    t=" ".join([str(now) for now in t])
+
+def mylog(*t, path='log.txt'):
+    t = " ".join([str(now) for now in t])
     print(t)
     if os.path.isfile(path) == False:
         f = open(path, 'w+')
@@ -18,6 +19,7 @@ def mylog(*t,path = 'log.txt'):
         f = open(path, 'a')
     f.write(t + '\n')
     f.close()
+
 
 def set_seed(seed):
     # torch.set_default_tensor_type(torch.cuda.FloatTensor)
@@ -27,3 +29,11 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     random.seed(seed)
+
+
+def get_device():
+    if torch.backends.mps.is_built():
+        device = torch.device('mps')
+    else:
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return device
